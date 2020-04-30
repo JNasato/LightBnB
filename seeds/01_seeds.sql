b060287e1a302bab8fccc69380027d4ce9484c76
Jeremy Nasato <jeremy.nasato12@gmail.com>
2020-04-30 19:37:29 +0000

    change users functions to use postgres queries with promises

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -4,9 +4,9 @@ INSERT INTO users (name, email, password)
   ('Nick Kroll', 'hormone_monster@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
 INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
-  VALUES (2, 'Johns Place', 'description', 'https://sqlzoo.net/w/images/5/50/Movie2-er.png', 'https://sqlzoo.net/w/images/c/ce/FootballERD.png', 36500, 2, 6, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland and Labrador', 44583, true),
-  (3, 'Bouncy Castle', 'description', 'https://sqlzoo.net/w/images/5/50/Movie2-er.png', 'https://sqlzoo.net/w/images/c/ce/FootballERD.png', 15000, 6, 2, 1, 'Canada', '513 Powov Grove', 'Jaebvap', 'Ontario', '38051', true),
-  (2, 'HUSH', 'description', 'https://sqlzoo.net/w/images/5/50/Movie2-er.png', 'https://sqlzoo.net/w/images/c/ce/FootballERD.png', 22200, 0, 1, 5, 'Canada', '1392 Gaza Junction', 'Upetafpuv', 'Nova Scotia', '81059', true);
+  VALUES (2, 'Johns Place', 'description', 'https://mediad.publicbroadcasting.net/p/shared/npr/202001/706836850.jpg', 'https://sqlzoo.net/w/images/c/ce/FootballERD.png', 42000, 2, 6, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland and Labrador', 44583, true),
+  (3, 'Bouncy Castle', 'description', 'https://i.pinimg.com/originals/45/6a/52/456a52031610e8fb2998705c35f03264.jpg', 'https://sqlzoo.net/w/images/c/ce/FootballERD.png', 900, 6, 2, 1, 'Canada', '513 Powov Grove', 'Jaebvap', 'Ontario', '38051', true),
+  (2, 'HUSH', 'description', 'https://pbs.twimg.com/profile_images/530054996423749632/uIa2HxTV.jpeg', 'https://sqlzoo.net/w/images/c/ce/FootballERD.png', 66669, 0, 1, 5, 'Canada', '1392 Gaza Junction', 'Upetafpuv', 'Nova Scotia', '81059', true);
 
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
   VALUES ('2018-09-11', '2018-09-26', 3, 1),
